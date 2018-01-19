b02b8d0639aef95b81ccf57b5ae4bc0832bfcdb2
freezy <marek.viger@gmail.com>
2018-01-19 11:03:58 +0100

    Dont' skip tests on PHP 5.4+

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -10,10 +10,6 @@ abstract class BaseTestCase extends TestCase
 
     public function testShouldNotThrowSyntaxErrorOnPhp53()
     {
-        if (version_compare(PHP_VERSION, '5.4.0', '>=')) {
-            $this->markTestSkipped('Version greater than 5.3, skipping...');
-        }
-
         $helper = new $this->helperClassName;
     }
 }
