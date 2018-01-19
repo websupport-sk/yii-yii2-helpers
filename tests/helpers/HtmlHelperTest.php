bc4ef131b847650e760029a972a90dbbeb3b3849
freezy <marek.viger@gmail.com>
2018-01-19 16:31:26 +0100

    Test bridged exception in HTML helper

--- tests/helpers/HtmlHelperTest.php
+++ tests/helpers/HtmlHelperTest.php
@@ -3,8 +3,17 @@
 namespace tests\helpers;
 
 use tests\BaseTestCase;
+use yii\helpers\Html;
 
 class HtmlHelperTest extends BaseTestCase
 {
     protected $helperClassName = 'yii\helpers\BaseHtml';
+
+    /**
+     * @expectedException \CException
+     */
+    public function testThrowBridgedExceptionClass()
+    {
+        Html::getAttributeName('[invalid-attribute-name]');
+    }
 }
